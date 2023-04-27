d2038fe2df4082eccdf8f1be3a7c32895bb6ed26
YonghoBae <dyddyd134@chungbuk.ac.kr>
2023-04-27 19:02:46 +0900

    HW#6_third

--- singly-linked-list.c
+++ singly-linked-list.c
@@ -43,7 +43,7 @@ int main()
 	int key;
 	headNode* headnode=NULL;
 	
-	printf("[----- [Yongho Bae] [2020039008] -----]");
+	printf("[----- [Yongho Bae] [2020039008] -----]\n");
 
 	do{
 		printf("----------------------------------------------------------------\n");
@@ -143,11 +143,12 @@ int freeList(headNode* h){
  */
 int insertFirst(headNode* h, int key) {
 
-	listNode* node = (listNode*)malloc(sizeof(listNode));
-	node->key = key;
+	listNode* node = (listNode*)malloc(sizeof(listNode)); //추가할 node 메모리 할당
+	node->key = key; //추가할 node의 key값 대입
 
-	node->link = h->first;
-	h->first = node;
+	node->link = h->first; 
+	h->first = node; 
+	// node를 첫번째에 추가
 
 	return 0;
 }
@@ -155,32 +156,33 @@ int insertFirst(headNode* h, int key) {
 
 /* 리스트를 검색하여, 입력받은 key보다 큰값이 나오는 노드 바로 앞에 삽입 */
 int insertNode(headNode* h, int key) {
-	listNode* node = (listNode*)malloc(sizeof(listNode));
-	listNode* search = h->first;
-	node -> key = key;
-	node -> link = NULL;
+	listNode* node = (listNode*)malloc(sizeof(listNode)); //추가할 node 메모리 할당
+	listNode* search = h->first; //넣을 위치 검색을 위한 노드포인터 선언 및 처음노드를 가리키게 초기화
+	node -> key = key; //추가할 node의 key값 대입
+	node -> link = NULL; //node의 link 초기화
 
 	if(h->first == NULL){
 			h->first = node;
 			return 0;
-		}
+	} //비어있는 리스트면 바로 node 삽입 후 함수종료
 
 	while(search != NULL){
 			if(h->first->key > key){
 				node->link = h->first;
 				h->first = node;
 				break; 
-			}
+			} //추가할 key값이 가장 작아서 첫번째 노드 앞에 node를 삽입해야하는 경우
 			else if(search->link == NULL){
 				search->link = node;
 				break;
-			}
+			} //추가할 key값이 가장 커서 마지막 노드 뒤에 node를 삽입해야하는 경우
 			else if(search->link->key > key){
 				node->link = search->link;
 				search->link = node;
 				break;
-			}
-			search = search->link;
+			} //중간에 node를 삽입해야하는 경우
+
+			search = search->link; //다음 노드 검색을 위해
 	}
 
 	return 0;
@@ -190,25 +192,20 @@ int insertNode(headNode* h, int key) {
  * list에 key에 대한 노드하나를 추가
  */
 int insertLast(headNode* h, int key) {
-	listNode* node = (listNode*)malloc(sizeof(listNode));
-	listNode* search;
-	node->key = key;
-	node->link = NULL;
-	search = h->first;
+	listNode* node = (listNode*)malloc(sizeof(listNode)); //추가할 node 메모리 할당
+	listNode* search = h->first; //넣을 위치 검색을 위한 노드포인터 선언 및 처음노드를 가리키게 초기화
+	node->key = key; //추가할 node의 key값 대입
+	node->link = NULL; //node의 link 초기화
 
 	if(h->first == NULL){
 		h->first = node;
 		return 0;
-	}
+	} //비어있는 리스트면 바로 node 삽입 후 함수종료
 
-	while(search != NULL){	
-		if(search->link == NULL){
-			search->link = node;
-			break;
-		}
+	while(search->link != NULL){	
 		search = search->link;
-	}
-
+	} //마지막 노드 검색
+	search->link = node; //마지막 노드 이후에 node 삽입
 	return 0;
 }
 
@@ -217,13 +214,15 @@ int insertLast(headNode* h, int key) {
  * list의 첫번째 노드 삭제
  */
 int deleteFirst(headNode* h) {
-	listNode* temp = h->first;
+	listNode* temp = h->first; //첫번째 노드의 연결이 끊긴 후 메모리할당해체를 위한 노드포인터선언 및 초기화
+
 	if(h->first==NULL){
 		printf("비어있는 리스트입니다\n");
 		return 0;
-	}
-	h->first = h->first->link;
-	free(temp);
+	} // 비어있는 리스트면 함수종료
+
+	h->first = h->first->link; //헤드노드에서 두번째 노드로 연결 
+	free(temp); //첫번째였던 노드 메모리할당해제
 
 	return 0;
 }
@@ -233,31 +232,32 @@ int deleteFirst(headNode* h) {
  * list에서 key에 대한 노드 삭제
  */
 int deleteNode(headNode* h, int key) {
-	listNode* search= h->first;
-	listNode* temp= h->first;
+	listNode* search= h->first; //해당 key를 가지고있는 노드를 찾기 위한 노드포인터선언 및 초기화
+	listNode* temp= h->first; //연결이 끊긴 후 메모리할당해체를 위한 노드포인터선언 및 초기화
 
 	if(h->first==NULL){
 			printf("비어있는 리스트입니다\n");
 			return 0;
-		}
+	} //비어있는 리스트면 함수종료
 
 	while(search != NULL){
 		if(h->first->key == key){
 			h->first = h->first->link;
 			free(temp);
 			break;
-		}
+		} //첫번째 노드의 key가 맞을 경우 헤드노드에서 두번째 노드로 연결 후 메모리할당해제
 		else if(search->link == NULL){
 			printf("삭제할 key: %d가 없습니다.\n",key);
-		}
+			break;
+		} //리스트 안에 삭제할 key가 없는 경우 
 		else if(search->link->key == key){
 			temp = search->link;
 			search->link = search->link->link;
 			free(temp);
 			break;
-		}
+		} //그 이후에 key값을 가진 노드가 있을 경우 
 		
-		search = search->link;
+		search = search->link; // 다음 노드 검색을 위해서
 	}
 
 	return 0;
@@ -268,27 +268,27 @@ int deleteNode(headNode* h, int key) {
  * list의 마지막 노드 삭제
  */
 int deleteLast(headNode* h) {
-	listNode* prev = NULL;
-	listNode* search = h->first;
+	listNode* prev = NULL; //마지막노드 삭제 후 마지막인 노드 링크에 NULL 대입을 위한 노트포인터 
+	listNode* search = h->first; //마지막 노드 검색을 위한 노트포인터 선언 및 초기화
 	
 	if(h->first == NULL){
 		printf("비어있는 리스트입니다\n");
 		return 0;
-	}
+	} //비어있는 리스트일 경우 함수종료
 	
 	if(h->first->link == NULL){
 		free(h->first);
 		h->first = NULL;
 		return 0;
-	}
+	} //첫번째 노드가 마지막 노드일 경우(=노드가 하나일 경우), 해당 노드 메모리할당해제
 
 	while(search->link != NULL){
 		prev = search;
 		search = search->link;
-	}
+	} //마지막 노드 탐색(search->마지막노드, prev->마지막노드의 이전 노드)
 	
-	prev->link = NULL;
-	free(search);
+	prev->link = NULL; //마지막노드와 이전노드의 연결해제
+	free(search); //마지막노드 메모리할당 해제
 
 	return 0;
 }
@@ -298,23 +298,24 @@ int deleteLast(headNode* h) {
  * 리스트의 링크를 역순으로 재 배치
  */
 int invertList(headNode* h) {
-    listNode* prev = NULL;
-    listNode* curr = h->first;
-    listNode* next = NULL;
+    listNode* prev = NULL; //이전 노드포인터
+    listNode* curr = h->first; //현재 역으로 연결할 노드포인터
+    listNode* next = NULL; //다음 노드포인터
 
     if (h->first == NULL) {
         printf("비어있는 리스트입니다\n");
         return 0;
-    }
+    }//비어있는 리스트면 함수종료
 
     while (curr != NULL) {
-        next = curr->link;
-        curr->link = prev;
-        prev = curr;
-        curr = next;
-    }
+        next = curr->link; //curr 다음 노드를 next로 가리킴
+        curr->link = prev; 
+		//처음에는 현재에서 다음으로 가는 노드의 연결을 끊고 다음부터는 현재 노드를 이전 노드로 연결해줌
+        prev = curr; //현재 노드가 이전 노드가 되고
+        curr = next; //다음 노드가 현재 노드가 됨.
+    } //리스트의 끝까지 반복작업
 
-    h->first = prev;
+    h->first = prev; //prev에 남아있는 마지막노드를 헤더노드가 가리키게함.
 
     return 0;
 }
